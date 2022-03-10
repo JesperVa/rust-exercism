8bbfaa99ed7b96437fedfa5049d286ab560d453c
Jesper Vallett <jesper@vallett.se>
2022-03-10 17:51:20 +0100

    Additions to Assembly line

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -3,19 +3,15 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    (speed as f64 *221f64)
-}
-
-pub fn working_items_per_minute(speed: u8) -> u32 {
-    match speed {
-        0 => 0,
-        1..=4 => prod_per(speed, 1f64),
-        5..=8 => prod_per(speed, 0.9),
-        9..=10 => prod_per(speed, 0.77),
+    221.0 * (speed as f64) * match speed {
+        0 => 0f64,
+        1..=4 => 1f64,
+        5..=8 => 0.9,
+        9..=10 =>0.77,
         _ => unimplemented!()
     }
 }
 
-fn prod_per(speed: u8, percentage: f64) -> u32 {
-    (production_rate_per_hour(speed)/percentage) as u32
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    production_rate_per_hour(speed) as u32 / 60
 }
